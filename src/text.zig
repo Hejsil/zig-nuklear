4dda3ce0e61691ac2af94e671ee3ed193bb9ba7b
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-08-07 20:05:30 +0200

    Change Align struct to be an enum instead

--- src/text.zig
+++ src/text.zig
@@ -5,43 +5,24 @@ const std = @import("std");
 const mem = std.mem;
 const testing = std.testing;
 
-pub const Align = struct {
-    vertical: Vertical = .left,
-    horizontal: Horizontal = .middle,
-
-    pub const top_left: Alignment = .{ .vertical = .top, .horizontal = .left };
-    pub const mid_left: Alignment = .{ .vertical = .middle, .horizontal = .left };
-    pub const bot_left: Alignment = .{ .vertical = .bot, .horizontal = .left };
-    pub const top_center: Alignment = .{ .vertical = .top, .horizontal = .center };
-    pub const mid_center: Alignment = .{ .vertical = .middle, .horizontal = .center };
-    pub const bot_center: Alignment = .{ .vertical = .bot, .horizontal = .center };
-    pub const top_right: Alignment = .{ .vertical = .top, .horizontal = .right };
-    pub const mid_right: Alignment = .{ .vertical = .middle, .horizontal = .right };
-    pub const bot_right: Alignment = .{ .vertical = .bot, .horizontal = .right };
-
-    pub const Vertical = enum(u8) {
-        top = c.NK_TEXT_ALIGN_TOP,
-        middle = c.NK_TEXT_ALIGN_MIDDLE,
-        bottom = c.NK_TEXT_ALIGN_BOTTOM,
-    };
-
-    pub const Horizontal = enum(u8) {
-        left = c.NK_TEXT_ALIGN_LEFT,
-        center = c.NK_TEXT_ALIGN_CENTERED,
-        right = c.NK_TEXT_ALIGN_RIGHT,
-    };
-
-    pub fn toNuklear(flags: Align) nk.Flags {
-        return @enumToInt(flags.horizontal) | @enumToInt(flags.vertical);
-    }
+pub const Align = enum(u8) {
+    top_left = c.NK_TEXT_ALIGN_TOP | c.NK_TEXT_ALIGN_LEFT,
+    mid_left = c.NK_TEXT_ALIGN_MIDDLE | c.NK_TEXT_ALIGN_LEFT,
+    bot_left = c.NK_TEXT_ALIGN_BOTTOM | c.NK_TEXT_ALIGN_LEFT,
+    top_center = c.NK_TEXT_ALIGN_TOP | c.NK_TEXT_ALIGN_CENTERED,
+    mid_center = c.NK_TEXT_ALIGN_MIDDLE | c.NK_TEXT_ALIGN_CENTERED,
+    bot_center = c.NK_TEXT_ALIGN_BOTTOM | c.NK_TEXT_ALIGN_CENTERED,
+    top_right = c.NK_TEXT_ALIGN_TOP | c.NK_TEXT_ALIGN_RIGHT,
+    mid_right = c.NK_TEXT_ALIGN_MIDDLE | c.NK_TEXT_ALIGN_RIGHT,
+    bot_right = c.NK_TEXT_ALIGN_BOTTOM | c.NK_TEXT_ALIGN_RIGHT,
 };
 
 pub fn label(ctx: *nk.Context, text: []const u8, alignment: Align) void {
-    return c.nk_label(ctx, nk.slice(text), alignment.toNuklear());
+    return c.nk_label(ctx, nk.slice(text), @enumToInt(alignment));
 }
 
 pub fn labelColored(ctx: *nk.Context, text: []const u8, alignment: Align, color: nk.Color) void {
-    return c.nk_label_colored(ctx, nk.slice(text), alignment.toNuklear(), color);
+    return c.nk_label_colored(ctx, nk.slice(text), @enumToInt(alignment), color);
 }
 
 pub fn labelWrap(ctx: *nk.Context, text: []const u8) void {
