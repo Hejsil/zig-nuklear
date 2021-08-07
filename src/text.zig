fdd0800022bfa8c19a9ea3a8279ecc24f48c9e64
Patrice Freydiere <frett27@gmail.com>
2021-08-07 16:41:33 +0200

    add text struct

--- src/text.zig
+++ src/text.zig
@@ -5,14 +5,35 @@ const std = @import("std");
 const mem = std.mem;
 const testing = std.testing;
 
-const Type = c.nk_text_alignment;
+pub const TextHorizontalAlignment = enum(c_uint) { Left = 0x01, // NK_TEXT_ALIGN_LEFT
+Centered = 0x02, // NK_TEXT_ALIGN_CENTERED
+Right = 0x04 // NK_TEXT_ALIGN_RIGHT
+};
 
-pub fn label(ctx: *nk.Context, text: []const u8, textalign: Type) void {
-    return c.nk_label(ctx, nk.slice(text), @intCast(c_uint,@enumToInt(textalign)));
+pub const TextVerticalAlignment = enum(c_uint) { Top = 0x08, // NK_TEXT_ALIGN_TOP
+Middle = 0x10, // NK_TEXT_ALIGN_MIDDLE
+Bottom = 0x20 // NK_TEXT_ALIGN_BOTTOM
+};
+
+pub const TextAlignment = struct {
+    horizontal: TextHorizontalAlignment = Middle,
+    vertical: TextVerticalAlignment = Left,
+
+    pub fn toNuklear(flags: TextAlignment) nk.Flags {
+        return @enumToInt(flags.horizontal) | @enumToInt(flags.vertical);
+    }
+};
+
+pub const TextAlignmentLeft: TextAlignment = .{ .horizontal = .Left, .vertical = .Middle };
+pub const TextAlignmentCentered: TextAlignment = .{ .horizontal = .Centered, .vertical = .Middle };
+pub const TextAlignmentRight: TextAlignment = .{ .horizontal = .Right, .vertical = .Middle };
+
+pub fn label(ctx: *nk.Context, text: []const u8, textalign: TextAlignment) void {
+    return c.nk_label(ctx, nk.slice(text), textalign.toNuklear());
 }
 
-pub fn labelColored(ctx: *nk.Context, text: []const u8, textalign: Type, color: nk.Color) void {
-    return c.nk_label_colored(ctx, nk.slice(text),  @intCast(c_uint,@enumToInt(textalign)), color);
+pub fn labelColored(ctx: *nk.Context, text: []const u8, textalign: TextAlignment, color: nk.Color) void {
+    return c.nk_label_colored(ctx, nk.slice(text), textalign.toNuklear(), color);
 }
 
 pub fn labelWrap(ctx: *nk.Context, text: []const u8) void {
